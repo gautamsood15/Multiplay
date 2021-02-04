c419246b065207e97391456a86f582160c9c5ee9
gautamsood15 <gauty22@gmail.com>
2021-02-04 15:47:50 +0530

    adding adding start pos to played function

--- client.py
+++ client.py
@@ -58,7 +58,7 @@ def main():
     run = True
     n = Network()
     startPos = read_pos(n.getPos())
-    p = Player(50,50,100,100,(0,255,0))
+    p = Player(startPos[0],startPos[1],100,100,(0,255,0))
     clock = pygame.time.Clock()
 
     while run:
