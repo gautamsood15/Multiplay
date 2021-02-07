b42fd220b8ff656d38e147fc5c80c7a94d7306fa
gautamsood15 <gauty22@gmail.com>
2021-02-07 13:19:04 +0530

    Adding val to initation procedure

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ s.listen(2)     # allows only 2 people to connect to server
 
 print("Waiting for a connection, Server Started")
 
-pos = [(),()]
+pos = [(0,0),(100,100)]
 
 
 def threaded_client(conn):
