3dcc92b68bcc9cbffe3b17824ed5e0bd297091db
gautamsood15 <gauty22@gmail.com>
2021-02-17 16:44:04 +0530

    setting up socket connection

--- server.py
+++ server.py
@@ -1,3 +1,24 @@
 import socket
-import _thread
+from _thread import *
 import sys
+
+
+server = ""
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+
+s.listen(2)     # allows only 2 people to connect to server
+
+print("Waiting for a connection, Server Started")
+
+
+
+
+
