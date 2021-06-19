7ebcc729374d6c502353e0eb4f22bc4ee9e322b3
gautamsood15 <gauty22@gmail.com>
2021-06-19 12:35:46 +0530

    Adding function to draw rectangel and fill screen + mouse control function

--- client.py
+++ client.py
@@ -8,6 +8,30 @@ pygame.display.set_caption("Client")
 
 clientNumber = 0
 
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.color = color
+        self.rect = (x,y,width,height)
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+
+    def mave(self):
+        keys = pygame.keys.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            pass
+        if keys[pygame.K_RIGHT]:
+            pass
+        if keys[pygame.K_UP]:
+            pass
+        if keys[pygame.K_DOWN]:
+            pass
+
 def redrawWindow():
 
     win.fill((255,255,255))
