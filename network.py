34fc400fe38706503b2e7e8a4968893d0fbc5cb7
gautamsood15 <gauty22@gmail.com>
2021-07-04 23:54:34 +0530

    removing decode from connect()

--- network.py
+++ network.py
@@ -15,7 +15,7 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return self.client.recv(2048)
         except:
             pass
 
