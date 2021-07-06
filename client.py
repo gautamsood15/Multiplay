fc2752afb2dfa1c7b9d44acbd3374865fb6b9f5a
gautamsood15 <gauty22@gmail.com>
2021-07-06 06:09:54 +0530

    creating client function

--- client.py
+++ client.py
@@ -1,37 +1,99 @@
 import pygame
 from network import Network
-from player import Player
+import pickle
+pygame.font.init()
 
-width = 500
-height = 500
+width = 700
+height = 700
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
-def redrawWindow(win, player, player2):
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
 
-    win.fill((255,255,255))
-    player.draw(win)
-    player2.draw(win)
-    pygame.display.update()
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 40)
+        text = font.render(self.text, 1, (255,255,255))
+        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        x2 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
+
+
+def redrawWindow(win, game, p):
+    win.fill((128,128,128))
+    pass
+
+btns = [Button("Rock", 50, 500, (0,0,0)), Button("Scissors", 250, 500, (255,0,0)), Button("Paper", 450, 500, (0,255,0))]
 
 def main():
     run = True
-    n = Network()
-    p = n.getP()
-
     clock = pygame.time.Clock()
+    n = Network()
+    player = int(n.getP())
+    print("You are player", player)
+
 
     while run:
-
         clock.tick(60)
-        p2 = n.send(p)
+        try:
+            game = n.send("get")
+        except:
+            run = False
+            print("couldn't get the game")
+            break
+
+        if game.bothWent():
+            redrawWindow()
+            pygame.time.delay(500)
+            try:
+                game = n.send("reset")
+            except:
+                run = False
+                print("Couldn't get game")
+                break
+
+            font = pygame.font.SysFont("comicsans", 90)
+            if (game.winner() == 1 and player == 1) or (game.winner() == 0 and player == 0):
+                text = font.render("You Won!", 1, (255,0,0))
+            elif game.winner == -1:
+                text = font.render("Tie Game!", 1, (255,0,0))
+            else:
+                text = font.render("You Lost...", 1, (255,0,0))
+
+            win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(2000)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-        p.move()
-        redrawWindow(win, p, p2)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for btn in btns:
+                    if btn.click(pos) and game.connected():
+                        if player == 0:
+                            if not game.p1Went:
+                                n.send(btn.text)
+                        else:
+                            if not game.p2Went:
+                                n.send(btn.text)
+
+
 
 
 if __name__ == "__main__":
